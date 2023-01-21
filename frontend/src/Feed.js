ec73225b6e714e088b0f599fe01c7cb4f5176d82
curious <vishal238128@gmail.com>
2023-01-21 18:04:01 +0530

    two frontend files added

---
+++ frontend/src/Feed.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from "react";
+import TweetBox from "./TweetBox";
+import Post from "./Post";
+import "./Feed.css";
+import FlipMove from "react-flip-move";
+import axios from 'axios';
+import {TwitterContractAddress} from "./config.js";
+import {ethers} from "ethers";
+import Twitter from "./utils/TwitterContract.json";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*} prop means property
+     prop pass as an object inside component
+     prop pass from parent to child comonent
+     also callback function can pass through prop from parent to child component
+     props are immutable
+
+
+
+*/}
